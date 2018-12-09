eaa76ccd8b8f95dd79357e0005a1298200d20e61
neso <nesosuke@gmail.com>
2018-12-09 12:36:48 +0900

    fix

--- install-mastodon.sh
+++ install-mastodon.sh
@@ -3,6 +3,7 @@
 #These steps below were executed on 8th Dec 2018; latest versions of Mastodon was "v2.6.5", of Ruby was "2.5.3", of yarn was "1.12.3".
 #Official Installaton Documentation; https://docs.joinmastodon.org/administration/installation/#install-fail2ban-so-it-blocks-repeated-login-attempts)
 
+cd $HOME
 
 # Install curl
 sudo apt update
