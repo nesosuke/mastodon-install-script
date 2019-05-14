37c5afaf454fa7486d6d5ff2e2d92fa98db58540
nesosuke <nesosuke@gmail.com>
2019-05-15 03:38:43 +0900

    add: gem install bundler

--- install-mastodon-part2.sh
+++ install-mastodon-part2.sh
@@ -27,6 +27,7 @@ cd live
 git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 
 # Install the dependencies
+gem install bundler
 bundle install \
   -j$(getconf _NPROCESSORS_ONLN) \
   --deployment --without development test
