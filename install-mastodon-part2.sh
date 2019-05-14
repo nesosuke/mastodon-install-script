ed2aab04496d2c14d3ac38ee927492466caf5b82
nesosuke <nesosuke@gmail.com>
2019-05-15 03:26:46 +0900

    fix: ruby2.6.0 -> 2.6.1

--- install-mastodon-part2.sh
+++ install-mastodon-part2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Resume installing ruby  
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install 2.6.0 
-rbenv global 2.6.0 
+rbenv install 2.6.1 
+rbenv global 2.6.1
 
 # Obtain&Install SSL cert
 cd && git clone https://github.com/letsencrypt/letsencrypt ~/git/letsencrypt
