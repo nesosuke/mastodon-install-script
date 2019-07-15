7c9817577c0efb400926b9b7af00621975da71ee
nesosuke <nesosuke@gmail.com>
2019-07-15 10:40:37 +0900

    不要行削除

--- update.sh
+++ update.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-#mastodon-update-script 
-#wrote by nesosuke
+## Run on only your responsibilitity.## 
 
-# 
-# Run on only your responsibilitity. 
-#
+SKIP_POST_DEPLOYMENT_MIGRATIONS=true
+
 
 # Pull Mastodon 
-cd ~/live && git pull 
-echo $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 5)
-read -p "Set Mastodon version updating to > " MASTODON_VERSION
-git checkout $MASTODON_VERSION
-
-
-# Conf values 
-SKIP_POST_DEPLOYMENT_MIGRATIONS=true
-RAILS_ENV=production
+cd ~/live
+git fetch 
+git checkout  $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) 
 
 
 # Update pkg(s) 
@@ -32,23 +24,8 @@ yarn install
 
 
 # Migrate  
-~/live/bin/tootctl cache clear
-bundle exec rails assets:clobber 
-bundle exec rails db:migrate 
-sudo systemctl restart mastodon-*.service 
-
-
-# Precompile
-bundle exec rails assets:precompile 
-bundle exec rails db:migrate  
-
-
-# Restart mastodon-*.service ###
-sudo systemctl restart mastodon-*.service nginx
-
-
-# Clear cache 
-~/live/bin/tootctl cache clear
-
-
-# EOF
+RAILS_ENV=production bundle exec rails assets:clobber 
+RAILS_ENV=production bundle exec rails db:migrate 
+RAILS_ENV=production bundle exec rails assets:precompile 
+sudo systemctl restart mastodon-*.service
+RAILS_ENV=production ~/live/bin/tootctl cache clear
