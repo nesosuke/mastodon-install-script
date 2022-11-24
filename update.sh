55951209da33735aee12d861a815c3c2cb320969
nesosuke <neso@neso.tech>
2022-11-24 23:06:08 +0900

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
-## Run on only your responsibilitity.## 
+## Run on only your responsibilitity.##
 
 SKIP_POST_DEPLOYMENT_MIGRATIONS=true
 export NODE_OPTIONS="--max-old-space-size=1024"
 
 
-# Pull Mastodon 
-cd ~/live   
-git fetch upstream
-git checkout main
-git merge upstream/main
+# Pull Mastodon
+cd ~/live
+git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 
 # Reget Yarnpkg pubkey
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
-# Update pkg(s) 
-sudo apt update -y 
-sudo apt upgrade -y 
-cd ~/.rbenv/plugins/ruby-build && git pull 
+# Update pkg(s)
+sudo apt update -y
+sudo apt upgrade -y
+cd ~/.rbenv/plugins/ruby-build && git pull
 printf N | rbenv install $(cat ~/live/.ruby-version)
 rbenv global $(cat ~/live/.ruby-version)
 cd ~/live
 gem update --system
 gem install bundler
 bundle update
-bundle install 
-sudo yarn install 
+bundle install
+yarn install
 
 
-# Migrate  
-RAILS_ENV=production bundle exec rails assets:clobber 
-RAILS_ENV=production bundle exec rails db:migrate 
-RAILS_ENV=production bundle exec rails assets:precompile 
+# Migrate
+RAILS_ENV=production bundle exec rails assets:clobber
+RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails assets:precompile
 sudo systemctl restart mastodon-*.service
 RAILS_ENV=production ~/live/bin/tootctl cache clear
 
 # Migrate again
-RAILS_ENV=production bundle exec rails db:migrate 
+RAILS_ENV=production bundle exec rails db:migrate
 sudo systemctl restart mastodon-*.service
