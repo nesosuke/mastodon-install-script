3e4e137c952a452a0d87907300d13ac0ebf8c2e5
nesosuke <neso@neso.tech>
2022-12-01 06:41:33 +0000

    fix

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ export NODE_OPTIONS="--max-old-space-size=1024"
 
 # Pull Mastodon
 cd ~/live
+git fetch
 git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 
 # Reget Yarnpkg pubkey
